ac1d82f12911dccc53d0d65c5ff218d66cdf46b4
GY1970 <gy1970@yahoo.com>
2018-11-25 11:41:56 -1000

    modified background to grey. added if statements.

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 console.log("First Project");
 console.log("Hello World!");
+
+var num = 3;
+
+if (num > 0 || num % 2 !== 0) {
+  console.log("positive even");
+} else {
+  console.log("positive odd");
+}
